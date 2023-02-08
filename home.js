e66c0475ea89c06484c13dd0139820438565d57f
DenzCoder <100281304+denzcoder@users.noreply.github.com>
2023-02-08 17:17:39 +0000

    Add files via upload

--- home.js
+++ home.js
@@ -6,3 +6,18 @@ function onToggleMenu(s){
     navLinks.classList.toggle('top-[9%]')
 }
 
+/* Parallax Scroll */
+
+var scroll = document.getElementById("about");
+window.addEventListener('scroll', function(){
+    var value = window.scrollY;
+
+    scroll.style.bottom = value + 0.5 + 'px';
+})
+
+/* Circle */
+const text = document.querySelector('.text p');
+text.innerHTML = text.innerText.split('').map(
+    (char, i) =>
+    `<span style="transform:rotate(${i * 8.3}deg)">${char}</span>`
+).join('');
