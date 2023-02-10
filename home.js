e567b54847641d140c3aa360a5369f64c7d01005
DenzCoder <100281304+denzcoder@users.noreply.github.com>
2023-02-10 18:40:32 +0000

    Add files via upload

--- home.js
+++ home.js
@@ -21,3 +21,20 @@ text.innerHTML = text.innerText.split('').map(
     (char, i) =>
     `<span style="transform:rotate(${i * 8.3}deg)">${char}</span>`
 ).join('');
+
+/* Scroll Reveal */
+ScrollReveal({
+    reset: true,
+    distance: '60px',
+    duration: 2500,
+    delay: 400
+});
+
+ScrollReveal().reveal('.quote-one', { delay: 500, origin: 'bottom'});
+ScrollReveal().reveal('.quote-two', { delay: 600, origin: 'bottom'});
+ScrollReveal().reveal('.info-box', { delay: 900, origin: 'right'});
+
+/* Why Choose Us Scroll in */
+ScrollReveal().reveal('.consultation', { delay: 800, origin: 'bottom'});
+ScrollReveal().reveal('.variety', { delay: 700, origin: 'bottom'});
+ScrollReveal().reveal('.dedicated', { delay: 600, origin: 'bottom'});
